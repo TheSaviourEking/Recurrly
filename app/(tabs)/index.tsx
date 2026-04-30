b40a81317e9a61a7ec49cb49a3e4d4fe51f496c5
TheSaviourEking <saviour.eking@cyberservices.com>
2026-04-30 04:51:45 +0100

    feat: add authentication screens for sign-in and sign-up

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -6,6 +7,9 @@ export default function Index() {
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
+
+      <Link href={"/(auth)/sign-in"} className="mt-4 rounded bg-primary text-white p-4">Go to Sign in</Link>
+      <Link href={"/(auth)/sign-up"} className="mt-4 rounded bg-primary text-white p-4">Go to Sign up</Link>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,12 @@ import "@/global.css";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
+  // return <Stack screenOptions={{headerShown: false}}/>;
+
+   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
+      <Stack.Screen name="(auth)" options={{ headerShown: false }}/>
+  </Stack>
+   )
 }
