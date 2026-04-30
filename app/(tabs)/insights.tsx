0965afb562e1898e5c99be2f3fa06f0899a52f8f
TheSaviourEking <saviour.eking@cyberservices.com>
2026-04-30 06:38:39 +0100

    feat: wrap tab screens with safe-area view and background styling

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from 'react-native'
+import { styled } from 'nativewind';
+import { Text } from 'react-native';
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 const insights = () => {
   return (
-    <View>
+    <SafeAreaView className='p-5 flex-1 bg-background'>
       <Text>insights</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from 'react-native';
+import { styled } from 'nativewind';
+import { Text } from 'react-native';
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 const settings = () => {
   return (
-    <View>
+    <SafeAreaView className='p-5 flex-1 bg-background'>
       <Text>settings</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
