1bb10a357c0296e478313f06066faf443ecdffc3
TheSaviourEking <saviour.eking@cyberservices.com>
2026-05-02 02:52:28 +0100

    fix: correct Text nesting and daysLeft label in UpcomingSubscriptionCard

--- app/components/UpcomingSubscriptionCard.tsx
+++ app/components/UpcomingSubscriptionCard.tsx
@@ -5,13 +5,15 @@ import { Image, Text, View } from 'react-native'
 const UpcomingSubscriptionCard = ({ name, price, daysLeft, icon, currency}: UpcomingSubscription) => {
   return (
     <View className='upcoming-card'>
-      <Text className='upcoming-row'>
+      <View className='upcoming-row'>
         <Image source={icon} className="upcoming-icon"/>
         <View>
             <Text className='upcoming-price'>{formatCurrency(price, currency)}</Text>
-            <Text className='upcoming-meta' numberOfLines={1}>{daysLeft > 1 ? `${daysLeft} days left`: "Last day"} days left</Text>
+            <Text className='upcoming-meta' numberOfLines={1}>
++              {daysLeft > 1 ? `${daysLeft} days left` : "Last day"}
++            </Text>
         </View>
-      </Text>
+      </View>
 
       <Text className='upcoming-name' numberOfLines={1}>{name}</Text>
     </View>
